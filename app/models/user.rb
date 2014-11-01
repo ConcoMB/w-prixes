70246a197fb1e7f944bb65c78b4c5e57be11220b
ConcoMB <cmaderblanco@gmail.com>
2014-11-01 12:28:21 -0300

    mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_initialize :init
+
+  def init
+    return if password.present?
+    pass = Digest::SHA1.hexdigest(email + Time.current.to_s)
+    self.password = pass
+    self.password_confirmation = pass
+  end
+
 end
